4e9343ac996d5e16bf0f0e62424a2f20c90804bd
TwinTailsX <jazzysenpai@gmail.com>
2015-08-05 15:05:09 +0100

    UPDATE 0.3.4
    Use async to add jquery events

--- JACR Mod Dashboard.js
+++ JACR Mod Dashboard.js
@@ -1,7 +1,5 @@
 // Just a Chill Room... Mod Dashboard //
 
-// Version: 0.3.3
-
 // Description: Created for 1-click modding in JACR.
 
 // Developed by: TwinTails
@@ -11,7 +9,7 @@
 
 // Variables //
 var jacr = {
-	version: "v0.3.3",
+	version: "v0.3.4",
 	mode: "skip",
 	open: false
 };
@@ -60,160 +58,161 @@ $.get("https://rawgit.com/TwinTailsX/JACR-Mod-Dashboard/master/dashboard.html", 
 		}
 
 		$("#jacr-version").text(jacr.version);
+
+		// Private //
+
+		// Buttons functions //
+
+		// Toggle skip/lskip
+		$("#jacr-skip").on("click", function () {
+			// Change mode
+			jacr.mode = "lockskip";
+			$("#jacr-skip").addClass("jacr-ui-active");
+			$("#jacr-lockskip").removeClass("jacr-ui-active");
+		});
+
+		$("#jacr-lockskip").on("click", function () {
+			// Change mode
+			jacr.mode = "lockskip";
+			$("#jacr-skip").removeClass("jacr-ui-active");
+			$("#jacr-lockskip").addClass("jacr-ui-active");
+		});
+
+		// Skip button
+		$("#jacr-theme").on("dblclick", function () {
+			if (jacr.mode = "skip") {
+				API.sendChat("!skip theme");
+			} else {
+				API.sendChat("!lockskip theme");
+			}
+		});
+
+		$("#jacr-forbidden").on("dblclick", function () {
+			if (jacr.mode = "skip") {
+				API.sendChat("!skip forbidden");
+			} else {
+				API.sendChat("!lockskip forbidden");
+			}
+		});
+
+		$("#jacr-op").on("dblclick", function () {
+			if (jacr.mode = "skip") {
+				API.sendChat("!skip op");
+			} else {
+				API.sendChat("!lockskip op");
+			}
+		});
+
+		$("#jacr-unpopular").on("dblclick", function () {
+			if (jacr.mode = "skip") {
+				API.sendChat("!skip unpopular");
+			} else {
+				API.sendChat("!lockskip unpopular");
+			}
+		});
+
+		$("#jacr-unavailable").on("dblclick", function () {
+			if (jacr.mode = "skip") {
+				API.sendChat("!skip unavailable");
+			} else {
+				API.sendChat("!lockskip unavailable");
+			}
+		});
+
+		$("#jacr-history").on("dblclick", function () {
+			if (jacr.mode = "skip") {
+				API.sendChat("!skip history");
+			} else {
+				API.sendChat("!lockskip history");
+			}
+		});
+
+		$("#jacr-nsfw").on("dblclick", function () {
+			if (jacr.mode = "skip") {
+				API.sendChat("!skip nsfw");
+			} else {
+				API.sendChat("!lockskip nsfw");
+			}
+		});
+
+		$("#jacr-motto").on("dblclick", function () {
+			if (jacr.mode = "skip") {
+				API.sendChat("!skip motto");
+			} else {
+				API.sendChat("!lockskip motto");
+			}
+		});
+
+		$("#jacr-quality").on("dblclick", function () {
+			if (jacr.mode = "skip") {
+				API.sendChat("!skip quality");
+			} else {
+				API.sendChat("!lockskip quality");
+			}
+		});
+
+		$("#jacr-mix").on("dblclick", function () {
+			if (jacr.mode = "skip") {
+				API.sendChat("!skip mix");
+			} else {
+				API.sendChat("!lockskip mix");
+			}
+		});
+
+		// Waitlist //
+		$("#jacr-lock").on("dblclick", function () {
+			API.sendChat("!lock");
+		});
+
+		$("#jacr-unlock").on("dblclick", function () {
+			API.sendChat("!unlock");
+		});
+
+		$("#jacr-ping").on("dblclick", function () {
+			API.sendChat("!ping");
+		});
+
+		// Input //
+		$("#jacr-kick").on("dblclick", function () {
+			var text = $("#jacr-input").text();
+			API.sendChat("!kick @" + text);
+		});
+
+		$("#jacr-mute").on("dblclick", function () {
+			var text = $("#jacr-input").text();
+			API.sendChat("!mute @" + text);
+		});
+
+		$("#jacr-ban").on("dblclick", function () {
+			var text = $("#jacr-input").text();
+			API.sendChat("!ban @" + text);
+		});
+
+		$("#jacr-mottoset").on("dblclick", function () {
+			var text = $("#jacr-input").text();
+			API.sendChat("!mottoset " + text);
+		});
+
+		// Misc //
+		$("#jacr-stats").on("dblclick", function () {
+			API.sendChat("!stats");
+		});
+
+		$("#jacr-history").on("dblclick", function () {
+			API.sendChat("!history");
+		});
+
+		$("#jacr-status").on("dblclick", function () {
+			API.sendChat("!status");
+		});
+
+		$("#jacr-link").on("dblclick", function () {
+			API.sendChat("!link");
+		});
+
 	});
 
 	console.log("JACR Mod Dashboard loaded.");
 }).fail(function (data) {
 		alert("Failed to load dashboard. Is GitHub down?");
 	});
-
-// Private //
-
-// Buttons functions //
-
-// Toggle skip/lskip
-$("#jacr-switchbox").on("click", "#jacr-skip", function () {
-	// Change mode
-	jacr.mode = "lockskip";
-	$("#jacr-skip").addClass("jacr-ui-active");
-	$("#jacr-lockskip").removeClass("jacr-ui-active");
-});
-
-$("#jacr-switchbox").on("click", "#jacr-lockskip", function () {
-	// Change mode
-	jacr.mode = "lockskip";
-	$("#jacr-skip").removeClass("jacr-ui-active");
-	$("#jacr-lockskip").addClass("jacr-ui-active");
-});
-
-// Skip button
-$("#jacr-theme").on("dblclick", function () {
-	if (jacr.mode = "skip") {
-		API.sendChat("!skip theme");
-	} else {
-		API.sendChat("!lockskip theme");
-	}
-});
-
-$("#jacr-forbidden").on("dblclick", function () {
-	if (jacr.mode = "skip") {
-		API.sendChat("!skip forbidden");
-	} else {
-		API.sendChat("!lockskip forbidden");
-	}
-});
-
-$("#jacr-op").on("dblclick", function () {
-	if (jacr.mode = "skip") {
-		API.sendChat("!skip op");
-	} else {
-		API.sendChat("!lockskip op");
-	}
-});
-
-$("#jacr-unpopular").on("dblclick", function () {
-	if (jacr.mode = "skip") {
-		API.sendChat("!skip unpopular");
-	} else {
-		API.sendChat("!lockskip unpopular");
-	}
-});
-
-$("#jacr-unavailable").on("dblclick", function () {
-	if (jacr.mode = "skip") {
-		API.sendChat("!skip unavailable");
-	} else {
-		API.sendChat("!lockskip unavailable");
-	}
-});
-
-$("#jacr-history").on("dblclick", function () {
-	if (jacr.mode = "skip") {
-		API.sendChat("!skip history");
-	} else {
-		API.sendChat("!lockskip history");
-	}
-});
-
-$("#jacr-nsfw").on("dblclick", function () {
-	if (jacr.mode = "skip") {
-		API.sendChat("!skip nsfw");
-	} else {
-		API.sendChat("!lockskip nsfw");
-	}
-});
-
-$("#jacr-motto").on("dblclick", function () {
-	if (jacr.mode = "skip") {
-		API.sendChat("!skip motto");
-	} else {
-		API.sendChat("!lockskip motto");
-	}
-});
-
-$("#jacr-quality").on("dblclick", function () {
-	if (jacr.mode = "skip") {
-		API.sendChat("!skip quality");
-	} else {
-		API.sendChat("!lockskip quality");
-	}
-});
-
-$("#jacr-mix").on("dblclick", function () {
-	if (jacr.mode = "skip") {
-		API.sendChat("!skip mix");
-	} else {
-		API.sendChat("!lockskip mix");
-	}
-});
-
-// Waitlist //
-$("#jacr-lock").on("dblclick", function () {
-	API.sendChat("!lock");
-});
-
-$("#jacr-unlock").on("dblclick", function () {
-	API.sendChat("!unlock");
-});
-
-$("#jacr-ping").on("dblclick", function () {
-	API.sendChat("!ping");
-});
-
-// Input //
-$("#jacr-kick").on("dblclick", function () {
-	var text = $("#jacr-input").text();
-	API.sendChat("!kick @" + text);
-});
-
-$("#jacr-mute").on("dblclick", function () {
-	var text = $("#jacr-input").text();
-	API.sendChat("!mute @" + text);
-});
-
-$("#jacr-ban").on("dblclick", function () {
-	var text = $("#jacr-input").text();
-	API.sendChat("!ban @" + text);
-});
-
-$("#jacr-mottoset").on("dblclick", function () {
-	var text = $("#jacr-input").text();
-	API.sendChat("!mottoset " + text);
-});
-
-// Misc //
-$("#jacr-stats").on("dblclick", function () {
-	API.sendChat("!stats");
-});
-
-$("#jacr-history").on("dblclick", function () {
-	API.sendChat("!history");
-});
-
-$("#jacr-status").on("dblclick", function () {
-	API.sendChat("!status");
-});
-
-$("#jacr-song-link").on("dblclick", function () {
-	API.chatLog("Coming soon!");
-});
